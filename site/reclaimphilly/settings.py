36b446241fae5a923b8b23e2bf0273adadf457cd
Jeff Maher <jeff.m.maher@gmail.com>
2012-11-18 15:00:33 -0500

    Changed some names to play friendlier with the Eclipse Pydev/Django plugin

--- site/reclaimphilly/settings.py
+++ site/reclaimphilly/settings.py
@@ -99,10 +99,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = 'reclaimphilly.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = 'reclaimphilly.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
