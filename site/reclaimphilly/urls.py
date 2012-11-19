92bdb42031fe5d503ae1d9db6e270a73114efbfd
Jeff Maher <jeff.m.maher@gmail.com>
2012-11-18 21:29:24 -0500

    Changed some function names and URLs

--- site/reclaimphilly/urls.py
+++ site/reclaimphilly/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^services/locations/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/radius/(?P<radius>[+-]?\d{1,5}(\.\d{1,13})?)', 'reclaimphilly_web.rest_services.get_locations'),
-    url(r'^services/locations/id/(?P<id>\d+)', 'reclaimphilly_web.rest_services.get_location_detail'),
+    url(r'^services/get/locations/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/radius/(?P<radius>[+-]?\d{1,5}(\.\d{1,13})?)', 'reclaimphilly_web.rest_services.get_locations'),
+    url(r'^services/get/location/id/(?P<id>\d+)', 'reclaimphilly_web.rest_services.get_location_by_id'),
 )
--- site/reclaimphilly_web/rest_services.py
+++ site/reclaimphilly_web/rest_services.py
@@ -8,15 +8,9 @@ from reclaimphilly_web.models import Location
 
 location_service = LocationService()
 
-def get_locations(request, latitude, longitude, radius=0):
+def get_locations_in_radius(request, latitude, longitude, radius=0):
     """
     Gets a list of locations_dict within a given mile radius.
-    
-    It is assumed that the argument contains an HTTP GET
-    request with the following parameters:
-    - latitude (decimal number)
-    - longitude (decimal number)
-    - mileradius (decimal number)
     
 	Returned, is a JSON-encoded list of dictionaries. Each dictionary
 	may have the following keys:
@@ -42,7 +36,19 @@ def get_locations(request, latitude, longitude, radius=0):
     
     return HttpResponse(json_result, mimetype="application/json", content_type="application/json; charset=utf8") # switch content_type to 'application/javascript for easier debug in browser
 
-def get_location_detail(request, id):
+def get_location_by_id(request, id):
+	"""
+	Gets a single Location by its database ID column
+
+	Returned, is a JSON-encoded location object with the following properties 
+	- id
+	- latitude
+	- longitude
+	- address
+	- picture (URL)
+	- description
+	- type   TODO This is coming back as the DB string, not the descriptive English version -- change that	
+	"""
 	location = Location.objects.get(id=id)
 	
 	location_dict = conversions.location_to_detailed_coordinate_dict(location)
