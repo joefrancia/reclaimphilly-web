c923a2d1480c096cc530055db463d87600f9c15b
Jeff Maher <jeff.m.maher@gmail.com>
2012-11-18 21:25:13 -0500

    Added ability to get a location's details by ID

--- site/reclaimphilly/urls.py
+++ site/reclaimphilly/urls.py
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^services/locations/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/radius/(?P<radius>[+-]?\d{1,5}(\.\d{1,13})?)', 'reclaimphilly_web.rest_services.get_locations'),
+    url(r'^services/locations/id/(?P<id>\d+)', 'reclaimphilly_web.rest_services.get_location_detail'),
 )
--- site/reclaimphilly_web/rest_services.py
+++ site/reclaimphilly_web/rest_services.py
@@ -4,12 +4,13 @@ from services import LocationService
 import validations
 import conversions
 import unicodedata
+from reclaimphilly_web.models import Location
 
 location_service = LocationService()
 
 def get_locations(request, latitude, longitude, radius=0):
     """
-    Gets a list of locations within a given mile radius.
+    Gets a list of locations_dict within a given mile radius.
     
     It is assumed that the argument contains an HTTP GET
     request with the following parameters:
@@ -28,27 +29,29 @@ def get_locations(request, latitude, longitude, radius=0):
 	- type   TODO This is coming back as the DB string, not the descriptive English version -- change that
 	
 	Example Request:
-	http://localhost:8000/services/locations/latitude/37.95/longitude/-70.00/radius/5
+	http://localhost:8000/services/locations_dict/latitude/37.95/longitude/-70.00/radius/5
     """
     
     # Perform search
-    locations = location_service.get_locations(float(latitude), float(longitude), float(radius))
-    locations_list = conversions.locations_to_detailed_coordinate_list(locations)
+    locations_dict = location_service.get_locations(float(latitude), float(longitude), float(radius))
+    locations_list = conversions.locations_to_detailed_coordinate_list(locations_dict)
     
     json_result = "[]"
-    if locations:
+    if locations_dict:
         json_result = json.dumps(locations_list) 
     
     return HttpResponse(json_result, mimetype="application/json", content_type="application/json; charset=utf8") # switch content_type to 'application/javascript for easier debug in browser
 
-#def get_detail(request):
-#	# Required parameters
-#	id = None
-#	
-#	# Get the parameters from the request and validate
-#	params = request.GET
-#	try:
-#		id = 
+def get_location_detail(request, id):
+	location = Location.objects.get(id=id)
+	
+	location_dict = conversions.location_to_detailed_coordinate_dict(location)
+	
+	json_result = "[]"
+	if location:
+		json_result = json.dumps(location_dict)
+	
+	return HttpResponse(json_result, mimetype="application/json", content_type="application/json; charset=utf8") # switch content_type to 'application/javascript for easier debug in browser
 	
 
 #def add_location(request):
