9c0c132714298ba615fcd74bda3d381f665796f8
Jeff Maher <jeff.m.maher@gmail.com>
2012-11-18 21:20:25 -0500

    Refined URL for getting locations

--- site/reclaimphilly/urls.py
+++ site/reclaimphilly/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^services/locations/latitude/(?P<latitude>\d+)/longitude/(?P<longitude>\d+)/radius/(?P<radius>\d+)', 'reclaimphilly_web.rest_services.get_locations'),
+    url(r'^services/locations/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/radius/(?P<radius>[+-]?\d{1,5}(\.\d{1,13})?)', 'reclaimphilly_web.rest_services.get_locations'),
 )
