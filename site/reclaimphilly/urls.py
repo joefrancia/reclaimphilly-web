4de78ba0cfb4b5f96e9b96310f8742bf5ebd05eb
Jeff Maher <jeff.m.maher@gmail.com>
2012-11-19 21:23:44 -0500

    Fixed the URL match for adding locations

--- site/reclaimphilly/urls.py
+++ site/reclaimphilly/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^services/get/locations/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/radius/(?P<radius>[+-]?\d{1,5}(\.\d{1,13})?)$', 'reclaimphilly_web.rest_services.get_locations_in_radius'),
     url(r'^services/get/location/id/(?P<id>\d+)', 'reclaimphilly_web.rest_services.get_location_by_id'),
-    url(r'^services/put/location/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/type/(?P<type>\w{1,4})(/address/(?P<address>(\w|.|\s)+))?(/description/(?P<description>(\w|\s|[.-])))?$', 'reclaimphilly_web.rest_services.add_location'), # This will take GET arguments for description, address, and picture (the optional params)
+    url(r'^services/put/location/latitude/(?P<latitude>[+-]?\d{1,2}(\.\d{1,13})?)/longitude/(?P<longitude>[+-]?\d{1,3}(\.\d{1,13})?)/type/(?P<type>\w{1,4})(/address/(?P<address>(\w|\.|\s)+))?(/description/(?P<description>(\w|\s|[.?!"_\'-]){1,200}))?$', 'reclaimphilly_web.rest_services.add_location'), # TODO This is a scary regular expression - revise later
 )
